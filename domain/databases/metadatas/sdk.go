90aeb4c76187ca98a4f084678abf91afd9373656
Steve Rodrigue <stev.rodr@gmail.com>
2024-06-20 03:14:52 -0400

    Added the metadata structs

--- domain/databases/metadatas/sdk.go
+++ domain/databases/metadatas/sdk.go
@@ -4,6 +4,23 @@ import (
 	"github.com/steve-care-software/historydb/domain/hash"
 )
 
+// NewBuilder creates a new metadata builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents a metadata builder
+type Builder interface {
+	Create() Builder
+	WithPath(path []string) Builder
+	WithName(name string) Builder
+	WithDescription(description string) Builder
+	Now() (MetaData, error)
+}
+
 // MetaData represents a database metadata
 type MetaData interface {
 	Hash() hash.Hash
