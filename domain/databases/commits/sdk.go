32147b83a55ee90f73de2b215b8371117d37fbaa
Steve Rodrigue <stev.rodr@gmail.com>
2024-06-20 03:11:10 -0400

    Added the commit structs

--- domain/databases/commits/sdk.go
+++ domain/databases/commits/sdk.go
@@ -5,6 +5,22 @@ import (
 	"github.com/steve-care-software/historydb/domain/hash"
 )
 
+// NewBuilder creates a new commit builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents a commit builder
+type Builder interface {
+	Create() Builder
+	WithExecutions(executions executions.Executions) Builder
+	WithParent(parent hash.Hash) Builder
+	Now() (Commit, error)
+}
+
 // Commit represents a commit
 type Commit interface {
 	Hash() hash.Hash
