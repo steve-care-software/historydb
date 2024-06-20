b25e40c87e0b9ca30003e505418c8b1a4ff156d7
Steve Rodrigue <stev.rodr@gmail.com>
2024-06-20 03:19:22 -0400

    Added the database structs

--- domain/databases/sdk.go
+++ domain/databases/sdk.go
@@ -6,6 +6,22 @@ import (
 	"github.com/steve-care-software/historydb/domain/hash"
 )
 
+// NewBuilder creates a new database builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents a database builder
+type Builder interface {
+	Create() Builder
+	WithHead(head commits.Commit) Builder
+	WithMetaData(metaData metadatas.MetaData) Builder
+	Now() (Database, error)
+}
+
 // Database represents a database
 type Database interface {
 	Hash() hash.Hash
