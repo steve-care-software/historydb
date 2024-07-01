bce1c2cd6cf3bb894ebe2c10c68c4f1b52723185
Steve Rodrigue <stev.rodr@gmail.com>
2024-07-01 14:19:11 -0400

    Added application unit test

--- applications/application_test.go
+++ applications/application_test.go
@@ -49,45 +49,101 @@ func TestApplication_beginWithInit_Success(t *testing.T) {
 	)
 
 	// init, begin, commit once, push
-	name := "My Name"
-	description := "This is a description"
-	pContext, err := application.BeginWithInit(path, name, description)
-	if err != nil {
-		t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
-		return
-	}
+	func() {
+		name := "My Name"
+		description := "This is a description"
+		pContext, err := application.BeginWithInit(path, name, description)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
 
-	err = application.Execute(*pContext, expectedBytes)
-	if err != nil {
-		t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
-		return
-	}
+		err = application.Execute(*pContext, expectedBytes)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
 
-	err = application.Commit(*pContext)
-	if err != nil {
-		t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
-		return
-	}
+		err = application.Commit(*pContext)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
 
-	err = application.Push(*pContext)
-	if err != nil {
-		t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
-		return
-	}
+		err = application.Push(*pContext)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
 
-	retDatabase, err := application.Retrieve(path)
-	if err != nil {
-		t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
-		return
-	}
+		retDatabase, err := application.Retrieve(path)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
 
-	if retDatabase.MetaData().Name() != name {
-		t.Errorf("the metaData name was expected to be '%s', '%s' returned", name, retDatabase.MetaData().Name())
-		return
-	}
+		if retDatabase.MetaData().Name() != name {
+			t.Errorf("the metaData name was expected to be '%s', '%s' returned", name, retDatabase.MetaData().Name())
+			return
+		}
 
-	if retDatabase.MetaData().Description() != description {
-		t.Errorf("the metaData description was expected to be '%s', '%s' returned", description, retDatabase.MetaData().Description())
-		return
-	}
+		if retDatabase.MetaData().Description() != description {
+			t.Errorf("the metaData description was expected to be '%s', '%s' returned", description, retDatabase.MetaData().Description())
+			return
+		}
+	}()
+
+	// begin, execute twice, commit, push
+	func() {
+		expectedFirstBytes := []byte("this is the second first bytes")
+		expectedSecondBytes := []byte("this is the second second bytes")
+		pContext, err := application.Begin(path)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
+
+		err = application.Execute(*pContext, expectedFirstBytes)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
+
+		err = application.Execute(*pContext, expectedSecondBytes)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
+
+		err = application.Commit(*pContext)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
+
+		err = application.Push(*pContext)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
+
+		retDatabase, err := application.Retrieve(path)
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
+
+		retCommit, err := application.RetrieveCommit(retDatabase.Head().Hash())
+		if err != nil {
+			t.Errorf("the error was expected to be nil, error returned: %s", err.Error())
+			return
+		}
+
+		list := retCommit.Executions().List()
+		if len(list) != 2 {
+			t.Errorf("the executions was expected to contain %d elements, %d returned", 2, len(list))
+			return
+		}
+	}()
+
 }
